d18e247a32cd6e56a112fb93074d763223eddbdb
hello1964 <DylanPusilo@gmail.com>
2020-10-20 13:04:43 +0000

    Added more stuff

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("Hello world")
+userInput = []
+userInput.append(input("What is your name? "))
+userInput.append(input("What is your favorite color? "))
+print("Hello %s, your favorite color is %s." % userInput)
